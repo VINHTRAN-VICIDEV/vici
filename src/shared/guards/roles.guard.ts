f6a0223227896aadadd70bb0c706d6feee5ff45b
Vinh Tran <vinhtran.vicidev@gmail.com>
2024-08-26 22:23:05 +0700

    improve(src): clean code and remove logs

--- src/shared/guards/roles.guard.ts
+++ src/shared/guards/roles.guard.ts
@@ -16,7 +16,6 @@ export class RolesGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    console.log("      console.log('payload', payload);");
     function matchRoles(roles: string[], userRole: string) {
       return roles.some((role) => role === userRole);
     }
